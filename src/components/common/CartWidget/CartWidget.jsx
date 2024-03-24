b73bfaba913fb91e0fbc6f676599ffef7967ded0
Martina Carreras <martina.carreras3@outlook.com>
2024-03-23 22:40:34 -0300

    Terminadisimo

--- src/components/common/CartWidget/CartWidget.jsx
+++ src/components/common/CartWidget/CartWidget.jsx
@@ -1,5 +1,5 @@
 import { FaShoppingCart } from "react-icons/fa";
-import { Badge } from "@mui/material";
+import { Badge, createTheme, ThemeProvider } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./CartWidget.css"
 import { useContext } from "react";
@@ -9,11 +9,27 @@ export const CartWidget = () => {
   const { totalItems } = useContext(CartContext);
   let total = totalItems();
 
+  const myTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#3F4924"
+      },
+      secondary: {
+        main: '#E0C2FF',
+        light: '#F5EBFF',
+        contrastText: '#47008F',
+      }
+    },
+  });
+
   return (
     <Link to="/cart">
-      <Badge badgeContent={total} showZero className="color" color="primary">
-        <FaShoppingCart className="color item"/>
-      </Badge>
+      <ThemeProvider theme={myTheme}>
+        <Badge badgeContent={total} showZero className="color" color="primary">
+          <FaShoppingCart className="color item"/>
+        </Badge>
+      </ThemeProvider>
+      
     </Link>
     )
 }
